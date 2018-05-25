9f1031ebc392d7204f23b6a0fb99a966e68db4f9
Vitalii Bobrov <bobrov1989@gmail.com>
2018-05-25 12:33:46 +0200

    Simple demo implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Popper from 'popper.js';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  // Case 1.
+  placement: Popper.Placement[] = [
+    'top',
+    'right',
+    'left',
+    'bottom'
+  ];
+  currentSide = this.placement[0];
+
+  // Case 2.
+  currentTarget = '#one';
+
+  // Case 3.
+  currentShow = false;
+
+  // Case 4.
+  shift: Popper.Placement[] = [
+    'left-start',
+    'left-end',
+    'right-start',
+    'right-end',
+    'top-start',
+    'top-end',
+    'bottom-start',
+    'bottom-end'
+  ];
+  currentShiftSide = this.shift[0];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { NgxPopper } from 'angular-popper';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    NgxPopper
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
